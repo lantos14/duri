e549d537bea70bdc8e06428369b66063a82509d1
lantos14 <lantos891213@gmail.com>
2018-11-24 14:32:20 +0100

    refact: migrated rp function

--- server.js
+++ server.js
@@ -1,65 +1,28 @@
-'use strict';
-
-const rp = require('request-promise');
-const express = require('express');
-const app = express();
-const path = require('path');
-const PORT = 3000;
-const cheerio = require('cheerio');
+const
+  express = require('express'),
+  getClothList = require('./src/rp'),
+  app = express(),
+  PORT = 3000;
 
 app.use(express.json());
 
-
-const options = {
-	url: 'https://www2.hm.com/hu_hu/noi/vasarlas-termek-szerint/ingek-es-bluzok.html',
-	headers: {
-		'Accept': 'text/html',
-	}
-}
-
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+app.get('/hm', async (req, res) => {
+  const options = {
+    url: 'https://www2.hm.com/hu_hu/noi/vasarlas-termek-szerint/ingek-es-bluzok.html',
+    headers: {
+      'Accept': 'text/html',
+    }
+  }
+
+  const result = await getClothList(options);
+  console.log(result);
+  res.json({
+    'data': result,
   });
-
-  app.get('/hm', (req, res) => {
-    const result = [];
-    let finished;
-	  
-	  rp(options)
-    
-    .then(html => {
-		  const $ = cheerio.load(html);
-		  let counter = 1;
-	
-			$('.products-listing').find('li').each(function (i, el) {
-				if ( i % 2 === 0) {
-					result.push({
-            'serialNum': counter,
-						'name': cheerio.load(el)(' article > .item-details > .item-heading > a').text(),
-						'price': cheerio.load(el)(' article > .item-details > .item-price > span').text(),
-						'img': cheerio.load(el)(' article > .image-container > a > .item-image').attr('src'),
-					})
-					counter++;
-				}
-			})
-    })
-    
-    .then(() => {
-      finished = true;
-
-    })
-
-		.catch(err => {
-			console.log(err);
-    });
-
-    const interval = setInterval(() => {
-      if (finished) {
-        res.json({
-          'data': result,
-        });
-        clearInterval(interval);
-      }
-    }, 500);
-  });
+});
 
