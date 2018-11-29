03cc76e44da51911e9ffa49ccd64d3f9d7c52438
lantost <lantos.tibor@kifu.gov.hu>
2018-11-29 16:48:31 +0100

    REFACT: changed then statements to async-await

--- src/getPromodList.js
+++ src/getPromodList.js
@@ -1,38 +1,24 @@
 const phantom = require("phantom");
-var _ph, _page, _outObj;
 
 const getPromodList = async () => {
 
-    let result;
+    const _ph = await phantom.create();
+    const _page = await _ph.createPage();
 
-    await phantom.create().then(function (ph) {
-        _ph = ph;
-        return _ph.createPage();
-
-    }).then(function (page) {
-        _page = page;
-        return _page.open('https://www.promod.hu/noi/pulover-kardigan/index.html');
-
-    }).then(function (status) {
-        console.log(status);
-        
-        const title = _page.evaluate(function(s) {
-            return document.querySelector(s).innerText;
-        }, '#resultatsSearch');
-
-        result = title;
-        return result;
-
-    }).then(function (content) {
-        _page.close();
-        _ph.exit();
-        result = content;
-
-    }).catch(function(e) {
-       console.log(e); 
+    await _page.on("onResourceRequested", function(requestData) {
+        console.info('Requesting', requestData.url)
     });
 
-    return result
+    const status = await _page.open('https://www.promod.hu/noi/pulover-kardigan/index.html');
+    console.log(status);
+
+    const result = await _page.evaluate(function(s) {
+        return document.querySelector(s).innerText;
+    }, '#resultatsSearch');
+
+    await _ph.exit();
+
+    return result;
 
 }
 
